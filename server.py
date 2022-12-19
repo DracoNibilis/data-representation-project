9bb0fa3b12f99b27cf0faee0889df29895df3916
DracoNibilis <mniamM@gmail.com>
2022-12-19 19:18:28 +0000

    web

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='pages')
 
 @app.route('/')
 def index():
     return "OOO"
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
